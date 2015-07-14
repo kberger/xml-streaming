db70e17d73674ce17b178f61e035b8c3c97b1c52
Karl <krab.berger@gmail.com>
2015-07-13 20:45:49 -0400

    Tried and failed to add 'skip on unacknowledged tags' functionality.

--- Xml.hs
+++ Xml.hs
@@ -1,13 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
-import Prelude hiding (readFile, writeFile)
-import Data.Map as M
+import Control.Monad.Trans.Resource
+import Data.Conduit (($$))
+import Data.Conduit.List (sinkNull)
+import Data.Text (Text, unpack)
 import Text.XML.Stream.Parse
-import Filesystem.Path.CurrentOS
+import Text.XML (Name, nameLocalName)
 
-main :: IO ()
+data Person = Person Int Text
+    deriving Show
+
+skipTagAndContents n = tagPredicate ((== n) . nameLocalName) ignoreAttrs (const Data.Conduit.List.sinkNull)
+
+isPerson :: Name -> Bool
+isPerson name = name == "person"
+
+parsePerson = tagPredicate isPerson (requireAttr "age") $ \age -> do
+    name <- content
+    return $ Person (read $ unpack age) name
+
+parsePeople = tagNoAttr "people" $ many parsePerson
+
 main = do
-    Document prologue root epilogue <- readFile def "Zawya.Sample.xml"
-
-    let context = empty
-
+    people <- runResourceT $
+            parseFile def "Person.xml" $$ force "people required" parsePeople
+    print people
